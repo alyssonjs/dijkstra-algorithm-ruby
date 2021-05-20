34ad6cca99fc7c7aef47c6cc09e52e8efb27d617
alysson <alysson.soares@hotmail.com.br>
2021-05-19 22:54:40 -0300

    fix logic

--- dijkstra.rb
+++ dijkstra.rb
@@ -1,33 +1,33 @@
 def calculate_cost(origin_vertex, target_vertex, matrix)
     max_value = 9999
     cost = []
-    adjacent = []
+    nearest_vertex = []
     closed_vertex = []
 
     matrix.each_with_index do |v, i|
         cost[i] = max_value
-        adjacent[i] = -1
+        nearest_vertex[i] = -1
         closed_vertex[i] = false
     end
 
     cost[origin_vertex] = 0
-    adjacent[origin_vertex] = -1
+    nearest_vertex[origin_vertex] = -1
     closed_vertex[origin_vertex] = true
 
     current_vertex = origin_vertex
 
     until current_vertex == target_vertex
         matrix.each_with_index do |v, i|
-            if matrix[current_vertex][i] != 0 && closed_vertex[i] != true
+            if !matrix[current_vertex][i].zero? && !closed_vertex[i]
                 if cost[current_vertex] + matrix[current_vertex][i] < cost[i]
-                    adjacent[i] = current_vertex
+                    nearest_vertex[i] = current_vertex
                     cost[i] = cost[current_vertex] + matrix[current_vertex][i]
                 end
             end
         end
         max_value = 9999
         matrix.each_with_index do |v, i|
-            if closed_vertex[i] == false && cost[i] < max_value
+            if !closed_vertex[i] && cost[i] < max_value
                 max_value = cost[i]
                 current_vertex = i
             end
@@ -35,10 +35,10 @@ def calculate_cost(origin_vertex, target_vertex, matrix)
         closed_vertex[current_vertex] = true
     end
 
-    print_values(current_vertex, target_vertex, cost, adjacent)
+    print_values(current_vertex, target_vertex, cost, nearest_vertex)
 end
 
-def print_values(current_vertex, target_vertex, cost, adjacent)
+def print_values(current_vertex, target_vertex, cost, nearest_vertex)
 
     print "Vertex, Cost, Path -> #{target_vertex}, #{cost[target_vertex]}, " 
 
@@ -48,7 +48,7 @@ def print_values(current_vertex, target_vertex, cost, adjacent)
 
     until current_vertex == -1
         values[k] = current_vertex
-        current_vertex = adjacent[current_vertex]
+        current_vertex = nearest_vertex[current_vertex]
         k += 1
     end
 
